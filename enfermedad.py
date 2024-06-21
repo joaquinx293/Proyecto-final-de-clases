429f053f4408425e534e080e32379879edf3cbaa
joaquinx293 <84052196+joaquinx293@users.noreply.github.com>
2024-06-21 18:19:15 -0400

    Add files via upload

---
+++ enfermedad.py
@@ -0,0 +1,15 @@
+
+class Enfermedad():
+
+    def infecciòn_probable():
+        print("listo")
+
+    def promedio_paso():
+        print("listo")
+
+
+    def enfermo():
+        print("listo")
+
+    def contador():
+        print("listo")
