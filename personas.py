7fa8c7627b8d6d5123f99bc7a5f62bca2c107f51
joaquinx293 <84052196+joaquinx293@users.noreply.github.com>
2024-06-25 22:07:26 -0400

    Update 6 personas.py

--- personas.py
+++ personas.py
@@ -112,7 +112,7 @@ def generar_personas(cantidad):
     for _ in range(cantidad):
         persona = Persona()
         nombre, apellido = persona.nombre_apellidos()
-        personas.append({'Nombre': nombre, 'Apellido': apellido,'ID': id[_], 'Estado': None, 'contador':None })
+        personas.append({'Nombre': nombre, 'Apellido': apellido,'ID': id[_], 'Estado': None, 'familia':None })
     return personas
 
 def generar_id(cantidad, longitud=10):
@@ -126,15 +126,23 @@ def generar_id(cantidad, longitud=10):
     return id
 def escribir_csv(personas, archivo_salida):
     with open(archivo_salida, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Nombre', 'Apellido','ID','Estado','contador']
+        fieldnames = ['Nombre', 'Apellido','ID','Estado','familia']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for persona in personas:
             writer.writerow(persona)
+def enfermedad(personas):
+    indice_infectado = random.randint(0, len(personas) - 1)
+    personas[indice_infectado]['Estado'] = 'Infectado'
+    print(f'Persona infectada: {personas[indice_infectado]["Nombre"]} {personas[indice_infectado]["Apellido"]}')
+def suptibilidad(personas ):
+    
+
 
 def main():
     cantidad_personas = 500
     personas = generar_personas(cantidad_personas)
+    enfermedad(personas)
     archivo_salida = 'personas.csv'
     escribir_csv(personas, archivo_salida)
     print(f'Se han generado {cantidad_personas} personas y se han guardado en {archivo_salida}')
