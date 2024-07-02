9fef5440ffa6930bdc7c6b69615471fff58b3433
joaquinx293 <84052196+joaquinx293@users.noreply.github.com>
2024-07-02 14:42:17 -0400

    Update9 personas.py

--- personas.py
+++ personas.py
@@ -3,7 +3,7 @@ import csv
 import random
 import json
 
-class Persona():
+class Persona:
     def nombre_apellidos(self):
         with open("nombres.json") as archivo:
             datos = json.load(archivo)
@@ -17,7 +17,14 @@ def generar_personas(cantidad):
     for _ in range(cantidad):
         persona = Persona()
         nombre, apellido = persona.nombre_apellidos()
-        personas.append({'Nombre': nombre, 'Apellido': apellido,'ID': id[_], 'Estado': None, 'familia':None })
+        personas.append({
+            'Nombre': nombre,
+            'Apellido': apellido,
+            'ID': id[_],
+            'Estado': 'Sano',  
+            'Familia': apellido,  
+            'Enfermedad': None, 
+        })
     return personas
 
 def generar_id(cantidad, longitud=20):
@@ -29,22 +36,19 @@ def generar_id(cantidad, longitud=20):
             ids_generados.add(nuevo_id)
             id.append(nuevo_id)
     return id
+
 def escribir_csv(personas, archivo_salida):
     with open(archivo_salida, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Nombre', 'Apellido','ID','Estado','familia']
+        fieldnames = ['Nombre', 'Apellido', 'ID', 'Estado', 'Familia','Enfermedad']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for persona in personas:
             writer.writerow(persona)
-def enfermedad(personas):
-    indice_infectado = random.randint(0, len(personas) - 1)
-    personas[indice_infectado]['Estado'] = 'Infectado'
-    print(f'Persona infectada: {personas[indice_infectado]["Nombre"]} {personas[indice_infectado]["Apellido"]}')
+
 
 def main():
     cantidad_personas = 1000
     personas = generar_personas(cantidad_personas)
-    enfermedad(personas)
     archivo_salida = 'personas_para_comunidad.csv'
     escribir_csv(personas, archivo_salida)
     print(f'Se han generado {cantidad_personas} personas y se han guardado en {archivo_salida}')
