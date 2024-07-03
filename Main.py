8d4137ee00f1c4ca4ea776e46fc9f7bf6f3c4ef7
joaquinx293 <84052196+joaquinx293@users.noreply.github.com>
2024-07-03 10:32:22 -0400

    Update Main 10 .py

--- Main.py
+++ Main.py
@@ -1,43 +1,37 @@
-from personas import generar_personas, escribir_csv
 from comunidad import Comunidad
 from enfermedad import Enfermedad
+from personas import generar_personas, escribir_csv
 
 if __name__ == "__main__":
     cantidad_personas = 1000
     personas = generar_personas(cantidad_personas)
     archivo_salida = 'personas_para_comunidad.csv'
     escribir_csv(personas, archivo_salida)
-    print(f'Se han generado {cantidad_personas} personas y se han guardado en {archivo_salida}')
+    beta = 0.1
+    gamma = 0.7
+    promedio_pasos = 20
     
-   
-    infeccion_probable = 0.1
-    Dias = 100
-    
-    enfermedad = Enfermedad(infeccion_probable, Dias)
-    
-    
-    num_comunidades = 100
+    enfermedad = Enfermedad(beta, gamma, promedio_pasos)
+    num_ciudadanos = 1000
     promedio_conexion_fisica = 5
     num_infectados = 10  
     probabilidad_conexion_fisica = 0.2
-    
-    
-    comunidad = Comunidad(num_comunidades, promedio_conexion_fisica, probabilidad_conexion_fisica)
+    comunidad = Comunidad(num_ciudadanos, promedio_conexion_fisica, probabilidad_conexion_fisica)
     archivo_csv = 'personas_para_comunidad.csv'
     comunidad.cargar_personas_desde_csv(archivo_csv)
     comunidad.crear_conexiones()
     enfermedad.inicializar_infectados(comunidad, num_infectados)
     
-    print("\nEstado inicial de la comunidad:")
+    print("Estado inicial de la comunidad:")
     comunidad.imprimir_estado()
-    
-    # Simulación de la propagación de la enfermedad por varios pasos
-    for paso in range(Dias):
-        print(f"\nDias {paso + 1}:")
+    for paso in range(promedio_pasos):
+        print(f"\nPaso {paso + 1}:")
         enfermedad.propagar_enfermedad(comunidad)
-        comunidad.imprimir_estado()
-        num_infectados = enfermedad.contar_infectados(comunidad)
-        print(f"Cantidad de infectados: {num_infectados}")
-    
+        #comunidad.imprimir_estado()
+        S, I, R = enfermedad.contar_sir(comunidad)
+        muertes = enfermedad.obtener_muertes()
+        print(f"Susceptibles: {S}, Infectados: {I}, Recuperados: {R}, Muertes: {muertes}")
     cantidad_personas = comunidad.numero_personas()
     print(f"\nNúmero total de personas: {cantidad_personas}")
+
+    # crear elimar lista pasos actual mas 5 llega ese paso 
