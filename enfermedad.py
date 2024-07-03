79e6c2e5082dbccff75f69be35ce44a3952b3fd5
joaquinx293 <84052196+joaquinx293@users.noreply.github.com>
2024-07-03 10:35:16 -0400

    Update enfermedad 10.py

--- enfermedad.py
+++ enfermedad.py
@@ -1,15 +1,14 @@
 import random
+
 class Enfermedad:
-    def __init__(self, infeccion_probable, promedio_pasos):
-        self.infeccion_probable = infeccion_probable
+    def __init__(self, beta, gamma, promedio_pasos):
+        self.beta = beta
+        self.gamma = gamma
         self.promedio_pasos = promedio_pasos
+        self.contador_muertes = 0  
     
     def inicializar_infectados(self, comunidad, num_infectados):
-        infectados_iniciales = []
-        while len(infectados_iniciales) < num_infectados:
-            indice = int(random.gauss(comunidad.num_comunidades / 2, comunidad.num_comunidades / 6))
-            if 0 <= indice < comunidad.num_comunidades and comunidad.personas[indice] not in infectados_iniciales:
-                infectados_iniciales.append(comunidad.personas[indice])
+        infectados_iniciales = random.sample(comunidad.personas, num_infectados)
         
         for infectado in infectados_iniciales:
             infectado['Estado'] = 'Infectado'
@@ -17,21 +16,38 @@ class Enfermedad:
     
     def propagar_enfermedad(self, comunidad):
         nuevos_infectados = []
+        nuevos_recuperados = []
+        
+        S, I, R = self.contar_sir(comunidad)
+        N = S + I + R
+        
         for persona in comunidad.personas:
             if persona['Estado'] == 'Infectado':
-                for conexion_id in persona['Conexiones']:
+                for conexion_id in persona['Conexiones'] + persona['ConexionesFamiliares']:
                     if random.random() < comunidad.probabilidad_conexion_fisica:
-                        for otra_persona in comunidad.personas:
-                            if otra_persona['ID'] == conexion_id and otra_persona['Estado'] == 'Sano':
-                                if random.random() < self.infeccion_probable:
-                                    nuevos_infectados.append(otra_persona)
-            persona['Contador'] -= 1
-            if persona['Contador'] <= 0 and persona['Estado'] == 'Infectado':
-                persona['Estado'] = 'De alta'
-        
+                        otra_persona = next((p for p in comunidad.personas if p['ID'] == conexion_id), None)
+                        if otra_persona and otra_persona['Estado'] == 'Sano':
+                            if random.random() < (self.beta * I / N):
+                                nuevos_infectados.append(otra_persona)
+
+                persona['Contador'] -= 1
+                if persona['Contador'] <= 0:
+                    if random.random() < self.gamma:
+                        persona['Estado'] = 'Recuperado'
+                        nuevos_recuperados.append(persona)
+                    else:
+                        persona['Estado'] = 'Muerto'
+                        self.contador_muertes += 1  
+
         for infectado in nuevos_infectados:
             infectado['Estado'] = 'Infectado'
             infectado['Contador'] = self.promedio_pasos
-    
-    def contar_infectados(self, comunidad):
-        return sum(1 for persona in comunidad.personas if persona['Estado'] == 'Infectado')
+
+    def contar_sir(self, comunidad):
+        S = sum(1 for persona in comunidad.personas if persona['Estado'] == 'Sano')
+        I = sum(1 for persona in comunidad.personas if persona['Estado'] == 'Infectado')
+        R = sum(1 for persona in comunidad.personas if persona['Estado'] == 'Recuperado')
+        return S, I, R
+
+    def obtener_muertes(self):
+        return self.contador_muertes
