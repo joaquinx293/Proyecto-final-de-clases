861a76041372313c22933da4e03d1ddf1eff31d9
joaquinx293 <84052196+joaquinx293@users.noreply.github.com>
2024-07-07 17:49:42 -0400

    Update Main 11.py

--- Main.py
+++ Main.py
@@ -1,21 +1,33 @@
+
 from comunidad import Comunidad
 from enfermedad import Enfermedad
 from personas import generar_personas, escribir_csv
+
+def simular_infeccion(comunidad, enfermedad, dias):
+    for dia in range(dias):
+        print(f"\nDía {dia + 1}:")
+        enfermedad.propagar_enfermedad(comunidad)
+        S, I, R = enfermedad.contar_sir(comunidad)
+        muertes = enfermedad.obtener_muertes()
+        print(f"Susceptibles: {S}, Infectados: {I}, Recuperados: {R}, Muertes: {muertes}")
 
 if __name__ == "__main__":
     cantidad_personas = 1000
     personas = generar_personas(cantidad_personas)
     archivo_salida = 'personas_para_comunidad.csv'
     escribir_csv(personas, archivo_salida)
-    beta = 0.1
-    gamma = 0.7
-    promedio_pasos = 20
+    
+    beta = 0.9
+    gamma = 0.1
+    promedio_pasos = 10
     
     enfermedad = Enfermedad(beta, gamma, promedio_pasos)
+    
     num_ciudadanos = 1000
     promedio_conexion_fisica = 5
     num_infectados = 10  
     probabilidad_conexion_fisica = 0.2
+    
     comunidad = Comunidad(num_ciudadanos, promedio_conexion_fisica, probabilidad_conexion_fisica)
     archivo_csv = 'personas_para_comunidad.csv'
     comunidad.cargar_personas_desde_csv(archivo_csv)
@@ -24,14 +36,10 @@ if __name__ == "__main__":
     
     print("Estado inicial de la comunidad:")
     comunidad.imprimir_estado()
-    for paso in range(promedio_pasos):
-        print(f"\nPaso {paso + 1}:")
-        enfermedad.propagar_enfermedad(comunidad)
-        #comunidad.imprimir_estado()
-        S, I, R = enfermedad.contar_sir(comunidad)
-        muertes = enfermedad.obtener_muertes()
-        print(f"Susceptibles: {S}, Infectados: {I}, Recuperados: {R}, Muertes: {muertes}")
+    
+    # Simulación de la infección durante 30 días
+    dias_simulacion = 100
+    simular_infeccion(comunidad, enfermedad, dias_simulacion)
+    
     cantidad_personas = comunidad.numero_personas()
     print(f"\nNúmero total de personas: {cantidad_personas}")
-
-    # crear elimar lista pasos actual mas 5 llega ese paso 
